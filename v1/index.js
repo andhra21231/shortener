959326730377a5568f127ccfaaac4843a4d19656
andhra21231 <andhrarifqi@icloud.com>
2022-03-28 15:55:21 +0700

    add the javascript

---
+++ v1/index.js
@@ -0,0 +1,7 @@
+const config = {
+    no_ref: "off", // no-reference
+    theme: "theme/default", // input with theme/<YourThemeName>
+    cors: "on", // Allow Cross-origin resource sharing for API requests
+    unique_links: true, // Remove duplicate links
+    custom_links: false, // Add custom links
+}
